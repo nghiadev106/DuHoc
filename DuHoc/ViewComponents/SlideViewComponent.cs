39c9302b9e2038bcd19a83744ada5b63abc51cea
nghiado.dev <nghiadv1006@gmail.com>
2022-05-23 08:58:23 +0700

    add slide component

---
+++ DuHoc/ViewComponents/SlideViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using DuHoc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebsiteBanHang.ViewComponents
+{
+    [ViewComponent(Name = "Slide")]
+    public class SlideViewComponent : ViewComponent
+    {
+        private readonly DuHocContext _context;
+
+        public SlideViewComponent(DuHocContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var slides = await _context.Slide.Where(x=>x.Status==1).ToListAsync();
+            return View("Index", slides);
+        }
+
+    }
+}
